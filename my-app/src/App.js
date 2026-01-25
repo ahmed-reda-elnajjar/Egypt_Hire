4b58307b32ad855ee14a79012bd3f2dc436660ff
Ahmed Alnajjar <a7mdelnagar297@gmail.com>
2026-01-25 22:27:11 +0200

    first

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TranslationsComponent from './translations'; 
+
+function App() {
+  return (
+    <div>
+      <TranslationsComponent />
+    </div>
+  );
+}
+
+export default App;
